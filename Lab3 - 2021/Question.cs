b3bb19b1ee5f153ab4f7eb7371b8e5a590644add
b13gusbe <gustav.bergstedt92@yahoo.se>
2021-10-28 01:34:32 +0200

    Created Classes Question and Quiz from the first draft of the UML-diagram.

---
+++ Lab3 - 2021/Question.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3___2021
+{
+    class Question
+    {
+        public string Statement { get; set; }
+        public string[] Answers { get; set; }
+
+        public readonly int CorrectAnswer;
+        public string Subject { get; set; }
+
+
+
+        public Question (string statement, int correctAnswer, string subject, params string[] answers)
+        {
+            Statement = statement;
+            Answers = answers;
+            CorrectAnswer = correctAnswer;
+            Subject = subject;
+        }
+
+    }
+}
---
+++ Lab3 - 2021/Quiz.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3___2021
+{
+    class Quiz
+    {
+        public List<Question> Questions;
+        public string Title;
+
+        private static readonly Random random = new();
+
+        public Quiz(string title)
+        {
+            Title = title;
+            Questions = new List<Question>();
+        }
+
+
+        public Question GetRandomQuestion()
+        {
+            int index = random.Next(0, Questions.Count);
+            return Questions[index];
+        }
+
+        public void AddQuestion(string statement, int correctAnswer, string subject, params string[] answers)
+        {
+            Questions.Add(new Question(statement, correctAnswer, subject, answers);
+        }
+
+        public void AddQuestion(Question question)
+        {
+            Questions.Add(question);
+        }
+
+        public void RemoveQuestion(int index)
+        {
+            Questions.RemoveAt(index);
+        }
+
+
+
+
+    }
+}
